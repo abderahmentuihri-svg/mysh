809574a9a1697e45d4011d6276402eb81a7a1dbd
dfbng <abderahmentuihri@gmail.com>
2026-07-27 22:32:02 +0100

    new commands added pwd and cd made the shell print your current directory before the $ prompt

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <unistd.h>
+#include <stdexcept>
+#include <limits.h>
 std::vector<std::string> split(std::string s,char delimiter) {
     std::vector<std::string> tokens;
     size_t pos = 0;
@@ -14,29 +17,63 @@ std::vector<std::string> split(std::string s,char delimiter) {
 
     return tokens;
 }
-int main(){
-	std::string line;
-	while (1){
-	std::cout<<"$ ";
-	std::getline(std::cin,line);
-	std::vector<std::string> parts=split(line, ' ');
-	std::string cmd=parts[0];
-	std::vector<std::string> args;
-	for (int i=1;i<parts.size();i++){
-		args.push_back(parts[i]);
-	}
-	if (cmd=="exit"){
-		break;
-	}
-	else if (cmd=="echo"){
-		for (int i=0;i<args.size();i++) {
-    			std::cout << args[i]<<'\n';
-		}
-	}
-	else{
-		std::cout<<line<<": command not found"<<std::endl;
-	}
-	}
-	return 0;
-	}
 
+std::string getcwd() {
+    char buffer[PATH_MAX];
+
+    if (::getcwd(buffer, sizeof(buffer)) != nullptr) {
+        return std::string(buffer);
+    } else {
+        throw std::runtime_error("getcwd failed");
+    }
+}
+int main(){
+    std::string line;
+
+    while (1){
+        std::cout << getcwd() << "$ ";
+        std::getline(std::cin, line);
+
+        std::vector<std::string> parts = split(line, ' ');
+        if (parts.empty() || parts[0].empty()) continue;
+
+        std::string cmd = parts[0];
+
+        std::vector<std::string> args;
+        for (int i = 1; i < parts.size(); i++){
+            args.push_back(parts[i]);
+        }
+
+        if (cmd == "exit"){
+            break;
+        }
+        else if (cmd == "echo"){
+            for (int i = 0; i < args.size(); i++) {
+                std::cout << args[i] << '\n';
+            }
+        }
+        else if (cmd == "cd"){
+            if (args.size() != 1){
+                std::cout << "cd: too many arguments" << std::endl;
+                continue;
+            }
+
+            if (chdir(args[0].c_str()) != 0) {
+                perror("cd");
+            }
+        }
+        else if (cmd == "pwd"){
+            if (args.size() != 0){
+                std::cout << "pwd: too many arguments" << std::endl;
+            }
+            else{
+                std::cout << getcwd() << std::endl;
+            }
+        }
+        else{
+            std::cout << line << ": command not found" << std::endl;
+        }
+    }
+
+    return 0;
+}
